95085249a81d206f8490ea046f9bac56bf10d51d
Tobshub <tobanigeorge@gmail.com>
2023-08-30 05:45:13 +0100

    support for default field prop

--- cmd/parser/table.go
+++ cmd/parser/table.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/tobshub/tobsdb/cmd/types"
@@ -15,25 +16,72 @@ func (table *Table) Create(d map[string]any) (map[string]any, error) {
 		data_type := fmt.Sprintf("%T", data)
 		switch field.builtin_type {
 		case types.Int:
-			if data_type == "float64" {
-				row[field.name] = int(data.(float64))
-			} else if data_type == "<nil>" && field.name == "id" {
-				row[field.name] = CreateId()
-			} else if data_type == "<nil>" && field.properties[types.Optional] == "true" {
-				row[field.name] = nil
-			} else {
-				return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+			{
+				switch data_type {
+				case "float64":
+					row[field.name] = int(data.(float64))
+				case "<nil>":
+					if default_val, ok := field.properties[types.Default]; ok {
+						if default_val == "auto" {
+							row[field.name] = CreateId()
+						} else {
+							str_int, err := strconv.ParseInt(default_val, 10, 0)
+							if err != nil {
+								return row, err
+							}
+							row[field.name] = str_int
+						}
+					} else if field.name == "id" {
+						row[field.name] = CreateId()
+					} else if field.properties[types.Optional] == "true" {
+						row[field.name] = nil
+					} else {
+						return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+					}
+				default:
+					return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+				}
+			}
+		case types.Float:
+			{
+				switch data_type {
+				case "float64":
+					row[field.name] = data.(float64)
+				case "<nil>":
+					if default_val, ok := field.properties[types.Default]; ok {
+						str_float, err := strconv.ParseFloat(default_val, 64)
+						if err != nil {
+							return row, err
+						}
+						row[field.name] = str_float
+					} else if field.properties[types.Optional] == "true" {
+						row[field.name] = nil
+					} else {
+						return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+					}
+				default:
+					return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+				}
 			}
 		case types.String:
-			if data_type == "string" {
-				row[field.name] = data.(string)
-			} else if data_type == "<nil>" && field.properties[types.Optional] == "true" {
-				row[field.name] = nil
-			} else {
-				return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+			{
+				switch data_type {
+				case "string":
+					row[field.name] = data.(string)
+				case "<nil>":
+					if default_val, ok := field.properties[types.Default]; ok {
+						row[field.name] = default_val
+					} else if field.properties[types.Optional] == "true" {
+						row[field.name] = nil
+					} else {
+						return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+					}
+				default:
+					return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+				}
 			}
 		case types.Date:
-			if data_type == "string" || data_type == "float64" {
+			{
 				switch data_type {
 				case "string":
 					val, err := time.Parse(time.RFC3339, data.(string))
@@ -44,11 +92,40 @@ func (table *Table) Create(d map[string]any) (map[string]any, error) {
 				case "float64":
 					val := time.UnixMilli(int64(data.(float64)))
 					row[field.name] = val
+				case "<nil>":
+					if default_val, ok := field.properties[types.Default]; ok {
+						if default_val == "now" {
+							row[field.name] = time.Now()
+						}
+					} else if field.properties[types.Optional] == "true" {
+						row[field.name] = nil
+					} else {
+						return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+					}
+				default:
+					return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
 				}
-			} else if data_type == "<nil>" && field.properties[types.Optional] == "true" {
-				row[field.name] = nil
-			} else {
-				return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+			}
+		case types.Bool:
+			{
+				switch data_type {
+				case "bool":
+					row[field.name] = data.(bool)
+				case "<nil>":
+					if default_val, ok := field.properties[types.Default]; ok {
+						if default_val == "true" {
+							row[field.name] = true
+						} else {
+							row[field.name] = false
+						}
+					} else if field.properties[types.Optional] == "true" {
+						row[field.name] = nil
+					} else {
+						return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+					}
+				default:
+					return row, InvalidFieldTypeError(data_type, table.Name, field.name, string(field.builtin_type))
+				}
 			}
 		}
 	}
@@ -56,8 +133,11 @@ func (table *Table) Create(d map[string]any) (map[string]any, error) {
 	return row, nil
 }
 
+var id_tracker int = 0
+
 func CreateId() int {
-	return 1
+	id_tracker++
+	return id_tracker
 }
 
 func InvalidFieldTypeError(invalid_type, table_name, field_name, field_type string) error {
