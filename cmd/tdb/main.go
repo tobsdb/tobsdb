4b9bce1177ec5e0a53ddda8b42a5bfb73d849dda
Tobshub <tobanigeorge@gmail.com>
2023-09-01 23:43:45 +0100

    ignored main by accident

---
+++ cmd/tdb/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	TobsdbBuilder "github.com/tobshub/tobsdb/internals/builder"
+	TobsdbParser "github.com/tobshub/tobsdb/internals/parser"
+)
+
+func main() {
+	cwd, _ := os.Getwd()
+	schema_path := cwd + "/schema.tdb"
+	db_write_path := cwd + "/db.tdb"
+
+	schema, _ := TobsdbParser.SchemaParser(schema_path)
+	db := TobsdbBuilder.NewTobsDB(&schema, db_write_path)
+
+	// default port 7085
+	log.Println("TobsDB listening on port 7085")
+	db.Listen(7085)
+}
