8f2cbddbd7e86bffee7d207feeda554ed97044fa
Tobshub <tobanigeorge@gmail.com>
2023-09-02 00:13:03 +0100

    port argument

--- cmd/tdb/main.go
+++ cmd/tdb/main.go
@@ -15,13 +15,13 @@ func main() {
 	schema_path := flag.String("schema", cwd+"/schema.tdb", "path to schema file")
 	db_write_path := flag.String("db", cwd+"/db.tdb", "path to save db data")
 	in_mem := flag.Bool("m", false, "don't persist db")
+	port := flag.Int("port", 7085, "listening port")
 
 	flag.Parse()
 
 	schema, _ := TobsdbParser.SchemaParser(*schema_path)
 	db := TobsdbBuilder.NewTobsDB(&schema, *db_write_path, *in_mem)
 
-	// default port 7085
-	log.Println("TobsDB listening on port 7085")
-	db.Listen(7085)
+	log.Println("TobsDB listening on port", *port)
+	db.Listen(*port)
 }
