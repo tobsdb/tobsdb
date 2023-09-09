1e15a0032a19b4f95a308078c834ecb00e8bee36
Tobshub <tobanigeorge@gmail.com>
2023-09-09 11:40:48 +0100

    flag to only validate schema

--- cmd/tdb/main.go
+++ cmd/tdb/main.go
@@ -16,12 +16,17 @@ func main() {
 	db_write_path := flag.String("db", cwd+"/db.tdb", "path to save db data")
 	in_mem := flag.Bool("m", false, "don't persist db")
 	port := flag.Int("port", 7085, "listening port")
+	parse_schema_only := flag.Bool("check", false, "check the schema file for errors and exit")
 
 	flag.Parse()
 
-	schema, _ := TobsdbParser.SchemaParser(*schema_path)
-	db := TobsdbBuilder.NewTobsDB(&schema, *db_write_path, *in_mem)
+	schema := TobsdbParser.SchemaParser(*schema_path)
+	if !(*parse_schema_only) {
+		db := TobsdbBuilder.NewTobsDB(&schema, *db_write_path, *in_mem)
 
-	log.Println("TobsDB listening on port", *port)
-	db.Listen(*port)
+		log.Println("TobsDB listening on port", *port)
+		db.Listen(*port)
+	} else {
+		log.Println("Schema checks completed")
+	}
 }
