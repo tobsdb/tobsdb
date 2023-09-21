f713952d58f35c280c780d74a249fa8832190381
Tobshub <tobanigeorge@gmail.com>
2023-09-21 22:08:45 +0100

    add parse vector prop func

--- internals/parser/props.go
+++ internals/parser/props.go
@@ -12,3 +12,11 @@ func ParseRelationProp(relation string) (string, string) {
 	return parsed_rel[0], parsed_rel[1]
 }
 
+func ParseVectorProp(value string) (TDBTypes.FieldType, int) {
+	parsed_val := strings.Split(value, ",")
+	vector_level, err := strconv.ParseInt(strings.TrimSpace(parsed_val[1]), 10, 0)
+	if err != nil || vector_level < 1 {
+		vector_level = 1
+	}
+	return TDBTypes.FieldType(parsed_val[0]), int(vector_level)
+}
