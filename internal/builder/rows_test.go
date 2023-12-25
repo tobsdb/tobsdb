19bf1d6809287e8a5add0876f6a33035a1b6ae3e
Tobshub <tobanigeorge@gmail.com>
2023-12-25 20:16:42 +0100

    test: tdbTableRow tests

---
+++ internal/builder/rows_test.go
@@ -0,0 +1,84 @@
+package builder_test
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+
+	. "github.com/tobsdb/tobsdb/internal/builder"
+	"gotest.tools/assert"
+)
+
+const TEST_SIZE = 10
+
+func newTestTDBTableRows() *TDBTableRows {
+	r := NewTDBTableRows()
+	for i := 0; i < TEST_SIZE; i++ {
+		r.Insert(i, TDBTableRow{SYS_PRIMARY_KEY: i})
+	}
+	return r
+}
+
+func TestTDBTableRows(t *testing.T) {
+	t.Run("Insert", func(t *testing.T) {
+		wg := sync.WaitGroup{}
+
+		r := NewTDBTableRows()
+
+		for i := 0; i < 10; i++ {
+			i := i
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				if r.Has(i) {
+					assert.Assert(t, false)
+				}
+				ok := r.Insert(i, TDBTableRow{SYS_PRIMARY_KEY: i})
+				assert.Assert(t, ok, fmt.Sprintf("failed to insert %d", i))
+			}()
+		}
+
+		wg.Wait()
+
+		for i := 0; i < 10; i++ {
+			i := i
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				ok := r.Insert(i, TDBTableRow{SYS_PRIMARY_KEY: i})
+				assert.Assert(t, !ok)
+			}()
+		}
+
+		wg.Wait()
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		r := newTestTDBTableRows()
+		for i := 0; i < TEST_SIZE; i++ {
+			assert.Assert(t, r.Has(i))
+			row, _ := r.Get(i)
+			assert.DeepEqual(t, row, TDBTableRow{SYS_PRIMARY_KEY: i})
+		}
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		r := newTestTDBTableRows()
+		i := rand.Intn(TEST_SIZE)
+		assert.Assert(t, r.Has(i))
+		ok := r.Delete(i)
+		assert.Assert(t, ok)
+	})
+
+	t.Run("Replace", func(t *testing.T) {
+		r := newTestTDBTableRows()
+		i := rand.Intn(TEST_SIZE)
+		assert.Assert(t, r.Has(i))
+		new_row := TDBTableRow{SYS_PRIMARY_KEY: i + TEST_SIZE}
+		r.Replace(i, new_row)
+
+		row, _ := r.Get(i)
+		assert.DeepEqual(t, row, new_row)
+	})
+}
