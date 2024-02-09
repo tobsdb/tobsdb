d732e2228979607a2f07446905be18931463a610
Tobshub <tobanigeorge@gmail.com>
2024-02-09 17:09:23 +0100

    misc(nodejs client): custom base error class

--- tools/client/js/src/errors.ts
+++ tools/client/js/src/errors.ts
@@ -4,24 +4,35 @@ enum Errors {
   ClosedClient = "ErrClosedClient",
 }
 
-export class DisconnectedError extends Error {
+class TdbError extends Error {
+  constructor(
+    public readonly name: string,
+    message: string,
+  ) {
+    super(message);
+  }
+}
+
+export class DisconnectedError extends TdbError {
   constructor() {
-    super("Not connected to a TobsDB server");
-    this.name = Errors.DisconnClient;
+    super(Errors.DisconnClient, "Not connected to a TobsDB server");
   }
 }
 
-export class CannotConnectError extends Error {
-  constructor(public readonly reason: string, stack?: string) {
-    super("Cannot connect to TobsDB server");
-    this.name = Errors.CannotConnectClient;
-    this.stack = stack;
+export class CannotConnectError extends TdbError {
+  constructor(
+    public readonly reason: string,
+    public readonly stack?: string,
+  ) {
+    super(Errors.CannotConnectClient, "Cannot connect to TobsDB server");
   }
 }
 
-export class ClosedError extends Error {
-  constructor(public readonly code: number, public readonly reason: string) {
-    super("TobsDB server closed connection");
-    this.name = Errors.ClosedClient;
+export class ClosedError extends TdbError {
+  constructor(
+    public readonly code: number,
+    public readonly reason: string,
+  ) {
+    super(Errors.ClosedClient, "TobsDB server closed connection");
   }
 }
