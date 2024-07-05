f274116e0501880ff331c4510bc95dc25e3057eb
Tobshub <tobanigeorge@gmail.com>
2024-07-05 18:51:58 +0100

    fix: ensure all tcp conn bytes are read

--- pkg/tcp.go
+++ pkg/tcp.go
@@ -10,16 +10,17 @@ const chunk_size = 1024 * 2
 
 func ConnReadBytes(conn net.Conn) ([]byte, error) {
 	var buf []byte
-	read_complete := false
 	tmp_buf := make([]byte, chunk_size)
-	for !read_complete {
+	for {
 		clear(tmp_buf)
 		n, err := conn.Read(tmp_buf)
-		if err == io.EOF && (n > 0 || len(buf) > 0) {
-			read_complete = true
-		}
-
 		if err == io.EOF {
+			if n > 0 {
+				buf = append(buf, tmp_buf[:n]...)
+				if n < chunk_size {
+					break
+				}
+			}
 			continue
 		}
 
@@ -29,7 +30,7 @@ func ConnReadBytes(conn net.Conn) ([]byte, error) {
 
 		buf = append(buf, tmp_buf[:n]...)
 		if n < chunk_size {
-			read_complete = true
+			break
 		}
 	}
 	return buf, nil
